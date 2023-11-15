63d2782558e3f31de0ef9c96ddca891fa76b4009
kolach <chikolad@gmail.com>
2023-11-15 15:28:15 -0500

    masking /dev/null

--- app/main.go
+++ app/main.go
@@ -6,12 +6,26 @@ import (
 	"os/exec"
 )
 
+type (
+	nullReader struct{}
+	nullWriter struct{}
+)
+
+func (nullReader) Read(p []byte) (int, error) {
+	return len(p), nil
+}
+
+func (nullWriter) Write(p []byte) (int, error) {
+	return len(p), nil
+}
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
 	cmd := exec.Command(command, args...)
+	cmd.Stdin = nullReader{}
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 
